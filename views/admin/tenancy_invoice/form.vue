c4d13b617d53fea9d135bfc11867a02ac44a323d
Dedan Irungu <dedanirungu@gmail.com>
2023-02-27 18:06:57 +0300

    improvement or params

--- views/admin/tenancy_invoice/form.vue
+++ views/admin/tenancy_invoice/form.vue
@@ -1,6 +1,6 @@
 <template>
   <edit-render
-    :path_param="path_param"
+    :path_param="['realestate', 'tenancy_invoice']"
     :form_fields="form_fields"
     :form_groups="form_groups"
     :model="model"
@@ -14,19 +14,6 @@ export default {
   data() {
     return {
       id: null,
-      path_param: ["realestate", "tenancy_invoice"],
-      form_groups: [
-        {
-          name: "setting",
-          legend: "Setting",
-          styleClasses: "flex col-lg-6 col-sm-12 setting-group"
-        },
-        {
-          name: "description",
-          legend: "Description",
-          styleClasses: "flex col-lg-6 col-sm-12 description-group"
-        }
-      ],
       model: {
         id: "",
         description: "",
@@ -37,52 +24,6 @@ export default {
         successful: ""
       },
 
-      form_fields: [
-        {
-          type: "editor",
-          name: "description",
-          group: "description",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "text",
-          name: "user_id",
-          group: "main",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "text",
-          name: "payment_id",
-          group: "main",
-          prefix: '"',
-          suffix: '"',
-          readonly: true,
-          disabled: true
-        },
-        {
-          type: "text",
-          name: "amount",
-          group: "main",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "yesno",
-          name: "completed",
-          group: "setting",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "yesno",
-          name: "successful",
-          group: "setting",
-          prefix: '"',
-          suffix: '"'
-        }
-      ],
     };
   }
 };
--- views/admin/unit_setup/form.vue
+++ views/admin/unit_setup/form.vue
@@ -1,10 +1,6 @@
 <template>
-  <edit-render
-    :path_param="path_param"
-    :form_fields="form_fields"
-    :form_groups="form_groups"
-    :model="model"
-  ></edit-render>
+  <edit-render :path_param="['realestate', 'unit_setup']" :form_fields="form_fields" :form_groups="form_groups"
+    :model="model"></edit-render>
 </template>
 
 <script>
@@ -14,19 +10,6 @@ export default {
   data() {
     return {
       id: null,
-      path_param: ["realestate", "unit_setup"],
-      form_groups: [
-        {
-          name: "setting",
-          legend: "Setting",
-          styleClasses: "flex col-lg-6 col-sm-12 setting-group"
-        },
-        {
-          name: "description",
-          legend: "Description",
-          styleClasses: "flex col-lg-6 col-sm-12 description-group"
-        }
-      ],
       model: {
         id: "",
         description: "",
@@ -37,52 +20,6 @@ export default {
         successful: ""
       },
 
-      form_fields: [
-        {
-          type: "editor",
-          name: "description",
-          group: "description",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "text",
-          name: "user_id",
-          group: "main",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "text",
-          name: "payment_id",
-          group: "main",
-          prefix: '"',
-          suffix: '"',
-          readonly: true,
-          disabled: true
-        },
-        {
-          type: "text",
-          name: "amount",
-          group: "main",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "yesno",
-          name: "completed",
-          group: "setting",
-          prefix: '"',
-          suffix: '"'
-        },
-        {
-          type: "yesno",
-          name: "successful",
-          group: "setting",
-          prefix: '"',
-          suffix: '"'
-        }
-      ],
     };
   }
 };
