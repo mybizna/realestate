2ee7324a70d9709418db99df5bb8fea18702b63d
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:14 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,92 @@
+<?php
+
+$this->add_right("realestate", "building", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "building", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "building", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "building", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "building", "registered", view:true, add:true);
+$this->add_right("realestate", "building", "guest", view:true, );
+
+$this->add_right("realestate", "building_unit_setup", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "building_unit_setup", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "building_unit_setup", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "building_unit_setup", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "building_unit_setup", "registered", view:true, add:true);
+$this->add_right("realestate", "building_unit_setup", "guest", view:true, );
+
+$this->add_right("realestate", "estate", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "estate", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "estate", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "estate", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "estate", "registered", view:true, add:true);
+$this->add_right("realestate", "estate", "guest", view:true, );
+
+$this->add_right("realestate", "reading_electricity", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_electricity", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_electricity", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_electricity", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "reading_electricity", "registered", view:true, add:true);
+$this->add_right("realestate", "reading_electricity", "guest", view:true, );
+
+$this->add_right("realestate", "reading_gas", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_gas", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_gas", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_gas", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "reading_gas", "registered", view:true, add:true);
+$this->add_right("realestate", "reading_gas", "guest", view:true, );
+
+$this->add_right("realestate", "reading_water", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_water", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_water", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "reading_water", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "reading_water", "registered", view:true, add:true);
+$this->add_right("realestate", "reading_water", "guest", view:true, );
+
+$this->add_right("realestate", "region", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "region", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "region", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "region", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "region", "registered", view:true, add:true);
+$this->add_right("realestate", "region", "guest", view:true, );
+
+$this->add_right("realestate", "tenancy", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "tenancy", "registered", view:true, add:true);
+$this->add_right("realestate", "tenancy", "guest", view:true, );
+
+$this->add_right("realestate", "tenancy_invoice", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy_invoice", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy_invoice", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy_invoice", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "tenancy_invoice", "registered", view:true, add:true);
+$this->add_right("realestate", "tenancy_invoice", "guest", view:true, );
+
+$this->add_right("realestate", "tenancy_service", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy_service", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy_service", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "tenancy_service", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "tenancy_service", "registered", view:true, add:true);
+$this->add_right("realestate", "tenancy_service", "guest", view:true, );
+
+$this->add_right("realestate", "town", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "town", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "town", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "town", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "town", "registered", view:true, add:true);
+$this->add_right("realestate", "town", "guest", view:true, );
+
+$this->add_right("realestate", "unit", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "unit", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "unit", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "unit", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "unit", "registered", view:true, add:true);
+$this->add_right("realestate", "unit", "guest", view:true, );
+
+$this->add_right("realestate", "unit_setup", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "unit_setup", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "unit_setup", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("realestate", "unit_setup", "staff", view:true, add:true, edit:true);
+$this->add_right("realestate", "unit_setup", "registered", view:true, add:true);
+$this->add_right("realestate", "unit_setup", "guest", view:true, );
